136ffe8fe0e1ada36702cc93a8ae25cc06e4f48b
Callum Gill <callum.gill.1993.cg@gmail.com>
2023-03-15 09:09:28 -0700

    Made a get request and listed out all countries

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <ul>
-      <li v-for="country in countries" :key="country.alpha3Code">{{ country.name }}</li>
+      <li v-for="country in countries" :key="country.alpha3Code">{{ country.name.common }}</li>
     </ul>
   </div>
 </template>
@@ -19,6 +19,7 @@ export default {
     axios.get('https://restcountries.com/v3.1/all')
       .then(response => {
         this.countries = response.data;
+        console.log(this.countries);
       })
       .catch(error => {
         console.log(error);
