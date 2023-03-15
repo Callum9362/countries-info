35fc4acdffbb74e92cbcfcbb67bd4c0e722ff681
Callum Gill <callum.gill.1993.cg@gmail.com>
2023-03-15 08:57:34 -0700

    Bootstrapped project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="country in countries" :key="country.alpha3Code">{{ country.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      countries: []
+    }
+  },
+  mounted() {
+    axios.get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        this.countries = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+}
+</script>
