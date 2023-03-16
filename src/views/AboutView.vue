cbb6ab216ecdec99f89c62005b05eb9413926cdd
Callum Gill <callum.gill.1993.cg@gmail.com>
2023-03-16 09:41:27 -0700

    Added in a formattedPopulation method

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <h1>{{ country.name.common }}</h1>
-    <p>Capital: {{ country.capital }}</p>
-    <p>Population: {{ country.population }}</p>
+    <p>Capital: {{ country.capital[0] }}</p>
+    <p>Population: {{ formattedPopulation }}</p>
     <p>Region: {{ country.region }}</p>
+    <p>Timezone: {{ country.timezones[0] }}</p>
   </div>
 </template>
 
@@ -17,9 +18,19 @@ export default {
       country: {},
     }
   },
+  computed: {
+    formattedPopulation() {
+      if (this.country.population) {
+        return this.country.population.toLocaleString();
+      } else {
+        return '';
+      }
+    }
+  },
   async created() {
     const response = await axios.get(`https://restcountries.com/v3.1/alpha/${this.$route.params.id}`);
     this.country = response.data[0];
+    console.log(this.country);
   },
 }
 </script>
